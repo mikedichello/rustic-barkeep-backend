39a527dd95516b402e848194fc4b26efd59d742b
mikedichello <michael.dichello@gmail.com>
2020-03-26 18:49:03 -0500

    build script fixed

--- deploy.js
+++ deploy.js
@@ -1,12 +1,36 @@
 const exec = require('child_process').exec;
 
-exec('npm run start', (err, stdout, stderr) => {
+
+exec('NODE_ENV=production npm run build', (err, stdout, stderr) => {
     if (err){
-        console.error(err);
+        console.error(err); 
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(err)
+            }, 1000);
+        })
     }else{
         console.log(stdout);
         console.log(stderr);
     };
-    return; 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('All Good');
+        }, 1000);
+    })
+    
+    .then(() => {
+        exec('NODE_ENV=production npm run start', (err, stdout, stderr) => {
+        if (err){
+            console.error(err);
+        }else{
+            console.log(stdout);
+            console.log(stderr);
+        };
+        return;    
+        })
+    });
 });
+
+
 
